7f80f0ab19c9de4cda7f71407022bf9eccc3dab9
Your Name <you@example.com>
2018-11-21 14:26:16 +0000

    1

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -30,7 +30,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # Facebook 
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(env["omniauth.auth"])
+    @user = User.find_for_oauth(env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
