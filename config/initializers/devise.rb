77fef38d4afbccb4fc1e5bad450d45af01eab5aa
Your Name <you@example.com>
2018-11-21 13:44:03 +0000

    facebook update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,6 +259,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :naver, ENV["NAVER_CLIENT_ID"], ENV["NAVER_ClIENT_SECRET"]
+  config.omniauth :facebook,ENV["FACEBOOK_CLIENT_ID"], ENV["FACEBOOK_CLIENT_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
